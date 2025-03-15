126bcd7546a626069ec94b7a4fa1a54dc7eea227
Nikole carrasquel <nikole.margarita.carrasquel@gmail.com>
2025-03-15 09:41:13 -0500

    us1/create register

---
+++ nomina.py
@@ -0,0 +1,23 @@
+##us1/crear registro
+users = [[],[]]
+
+def createUser():
+    user = []
+    id = int(input("Ingrese su documento de identidad"))
+    user.append(id)
+    user_name = input("Ingrese su nombre")
+    user.append(user_name)
+    phone = input("Ingrese su numero de telefono")
+    user.append(phone)
+    user_last_name = input("Ingrese su correo electronico.")
+    user.append(user_last_name)
+    user_password = input("Ingrese su contraseña")
+    user.append(user_password)
+    user.append(user)
+
+createUser()
+createUser()
+
+print(users)
+
+    
